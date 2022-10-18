944973ab03ef12cd6bd474eca0b31f08bded6e7f
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-19 03:51:14 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,27 @@
+export const item = {
+    "id": "ff3aac55-1247-48fa-ac61-024d768ff4fa",
+    "gallery": [
+        {
+            "id": "be102e95-d374-4afb-9a73-f3347d9369e8",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png"
+        },
+        {
+            "id": "948bdf96-3330-4e86-80c4-2e4f6591af41",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/301.png"
+        },
+        {
+            "id": "d0acc0b2-aa78-4f98-a523-7c9b5835c477",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+        },
+        {
+            "id": "cdd22173-d897-4cda-9a28-fe34c40fef90",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/201.png"
+        },
+        {
+            "id": "8147db88-c5e1-400d-90e0-1a5baf32ac26",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+        }
+    ],
+    "event_name": "from",
+    "event_description": "Account hold night relate on morning second. These happy set describe."
+}
